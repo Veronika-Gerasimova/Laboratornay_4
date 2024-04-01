432c13df93a6899c400398730197ada7d36bc8d5
Veronika <geras-veronika@rambler.ru>
2024-04-01 15:14:43 +0800

    Добавьте файлы проекта.

---
+++ Laboratornay_4/Form1.cs
@@ -0,0 +1,10 @@
+namespace Laboratornay_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Laboratornay_4/Plants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratornay_4
+{
+    public class Plants
+    {
+    }
+    public enum FlowersType { type1, type2 };
+    public enum FlowersColors { black, white, red, yellow, blue, green }
+    //Цветы
+    public class Flowers : Plants
+    {
+        public FlowersType type = FlowersType.type1;
+        public int numberOfPetals = 0;
+        public FlowersColors colors = FlowersColors.black;
+
+    }
+    public enum TreesType { coniferous, leafy };
+    //Деревья
+    public class Trees : Plants
+    {
+        public float height = 0;
+        public TreesType type = TreesType.coniferous;
+        public float radius = 0;
+
+    }
+    //Кустарники
+    public class Shrubs : Plants
+    {
+        public bool availabilityOfColors = false;
+        public int NumberOfBranches = 0;
+
+    }
+}
